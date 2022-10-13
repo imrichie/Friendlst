fae73ad536fa9e4b4e19bf62ef57324226548c25
Richie Flores <rfloresc@icloud.com>
2022-10-13 15:11:45 -0700

    Initial Commit

---
+++ Friendlst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friendlst
+//
+//  Created by Richie Flores on 10/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
