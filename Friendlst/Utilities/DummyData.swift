30136c4521a15e4088e8708bd8e9681881b4603a
Richie Flores <rfloresc@icloud.com>
2022-10-19 12:18:15 -0700

    Add compatibility checks for nav bar. Initialize dummy data. Configure TableView

---
+++ Friendlst/Utilities/DummyData.swift
@@ -0,0 +1,39 @@
+//
+//  DummyData.swift
+//  Friendlst
+//
+//  Created by Richie Flores on 10/19/22.
+//
+
+import Foundation
+
+class SamplePerson {
+    var firstName: String
+    var lastName: String
+    var city: String = "Unknown"
+    var age : Int = 0
+    
+    init(firstName: String, lastName: String, city: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.city = city
+    }
+}
+
+class SamplePersonManager {
+    var persons = [SamplePerson]()
+    
+    func loadData() {
+        let person1 = SamplePerson(firstName: "Sophie", lastName: "Hodges", city: "Glendale, AZ")
+        let person2 = SamplePerson(firstName: "Warren", lastName: "Stewart", city: "Riverside, CA")
+        let person3 = SamplePerson(firstName: "Abigail", lastName: "Sutherland", city: "San Francisco, CA")
+        let person4 = SamplePerson(firstName: "Ava", lastName: "Hill", city: "Austin, TX")
+        let person5 = SamplePerson(firstName: "Melanie", lastName: "Burgess", city: "Seattle, WA")
+        
+        persons.append(person1)
+        persons.append(person2)
+        persons.append(person3)
+        persons.append(person4)
+        persons.append(person5)
+    }
+}
--- Friendlst/ViewController.swift
+++ Friendlst/ViewController.swift
@@ -7,26 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    let dataManager = SamplePersonManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureNavbar()
+        
+        // load dummy data
+        dataManager.loadData()
+        
+        // verify data is loaded into array
+        if (!dataManager.persons.isEmpty) {
+            print("Number of Persons: \(dataManager.persons.count)")
+        }
+        
+        // configure the navigation bar
+        if #available(iOS 13, *) {
+            configureNavbar()
+        }
     }
     
+    // MARK: - Availability Features
+    @available(iOS 13, *)
     func configureNavbar() {
-        if #available(iOS 13.0, *) {
-            let navBarAppearance = UINavigationBarAppearance()
-            navBarAppearance.configureWithOpaqueBackground()
-            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-            navBarAppearance.backgroundColor = .tintColor
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.backgroundColor = .tintColor
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = .white
+    }
+    
+    //MARK: - TableView Delegates
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "personCell") {
+            let person = dataManager.persons[indexPath.row]
+            let personFullName = "\(person.lastName), \(person.firstName)"
             
-            navigationController?.navigationBar.standardAppearance = navBarAppearance
-            navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-            navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = .white
+            cell.textLabel?.text = personFullName
+            cell.detailTextLabel?.text = person.city
+            
+            return cell
         } else {
-            navigationController?.navigationBar.barTintColor = .tintColor
+            let defaultCell = UITableViewCell()
+            return defaultCell
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataManager.persons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110.0
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
