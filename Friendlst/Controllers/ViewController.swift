0d9e39b555e90421cba3e63329abeea232ca9427
Richie Flores <rfloresc@icloud.com>
2022-10-30 21:12:21 -0700

    Delta the Friend TableViewCell and delete associated logic for custom cell.

--- Friendlst/Controllers/ViewController.swift
+++ Friendlst/Controllers/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UITableViewController {
         
         dataManager.loadData()
         configureNavbar()
-        registerTableViewCells()
     }
     
     // MARK: - Helper Functions
@@ -26,34 +25,5 @@ class ViewController: UITableViewController {
         
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         navigationController?.navigationBar.standardAppearance = appearance
-    }
-    
-    func registerTableViewCells() {
-        let personCell = UINib(nibName: "FriendTableViewCell", bundle: nil)
-        self.tableView.register(personCell, forCellReuseIdentifier: CellNames.friendCell)
-    }
-    
-    //MARK: - TableView Delegates
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let friend = dataManager.persons[indexPath.row]
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: CellNames.friendCell) as! FriendTableViewCell
-        cell.nameLabel.text = "\(friend.lastName), \(friend.firstName)"
-        cell.cityLabel.text = friend.city
-        cell.birthdayLabel.text = "\(friend.birthDate)"
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataManager.persons.count
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100.0
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
