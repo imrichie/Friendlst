2efc2ad637208608a17d63788c292662a49166e7
Richie Flores <rfloresc@icloud.com>
2022-11-09 22:35:41 -0800

    Add validation to the textfields. Assign better responsiveness to being first responder and only allowing designated values for each entry.

--- Friendlst/Controllers/AddFriendViewController.swift
+++ Friendlst/Controllers/AddFriendViewController.swift
@@ -25,7 +25,7 @@ class AddFriendViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        stateText.delegate = self
+        setTextViewDelegates()
         
         if let friendToEdit = existingFriend {
             self.title = "Edit Friend"
@@ -85,6 +85,13 @@ class AddFriendViewController: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true)
     }
+    
+    func setTextViewDelegates() {
+        firstNameText.delegate = self
+        lastNameText.delegate = self
+        cityText.delegate = self
+        stateText.delegate = self
+    }
 }
 
 // MARK: - Extensions
@@ -97,10 +104,38 @@ extension UITextField {
 
 extension AddFriendViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let maxLength = 2
-        let currentString = (textField.text ?? "") as NSString
-        let newString = currentString.replacingCharacters(in: range, with: string)
-        
-        return newString.count <= maxLength
+        if textField == firstNameText || textField == lastNameText || textField == cityText {
+            let allowedCharacter = CharacterSet.letters
+            let allowedCharacter1 = CharacterSet.whitespaces
+            let characterSet = CharacterSet(charactersIn: string)
+            
+            return allowedCharacter.isSuperset(of: characterSet) || allowedCharacter1.isSuperset(of: characterSet)
+        } else if textField == stateText {
+            let allowedCharacter = CharacterSet.letters
+            let characterSet = CharacterSet(charactersIn: string)
+            
+            let currentString = (textField.text ?? "") as NSString
+            let newString = currentString.replacingCharacters(in: range, with: string)
+            
+            return newString.count <= 2 && allowedCharacter.isSuperset(of: characterSet)
+        } else {
+            return true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case firstNameText:
+            lastNameText.becomeFirstResponder()
+        case lastNameText:
+            cityText.becomeFirstResponder()
+        case cityText:
+            stateText.becomeFirstResponder()
+        case stateText:
+            commentsTextView.becomeFirstResponder()
+        default:
+            commentsTextView.becomeFirstResponder()
+        }
+        return true
     }
 }
