39b67108dd78792156e7084b6cf4432719d0ea06
Richie Flores <rfloresc@icloud.com>
2023-01-07 17:31:46 -0800

    Add new custom empty state view

---
+++ EmptyStateViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EmptyStateViewController.swift
+//  Friendlst
+//
+//  Created by Richie Flores on 1/7/23.
+//
+
+import UIKit
+
+class EmptyStateViewController: UIViewController {
+
+    
+    @IBAction func addNewFriendButton(_ sender: UIButton) {
+        print(">>> Adding new friend")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
